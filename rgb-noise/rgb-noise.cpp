107d02d80389d8fa5c18b27c07ed7e801f980f32
John Connor <jckusho@gmail.com>
2021-10-02 11:03:39 -0400

    More setup, added basic keyboard connection check

--- rgb-noise/rgb-noise.cpp
+++ rgb-noise/rgb-noise.cpp
@@ -1,6 +1,17 @@
-#include "../thirdparty/wooting-rgb-sdk/src/wooting-rgb-sdk.h"
+#include <iostream>
+#include "wooting-rgb-sdk.h"
 
 int main()
 {
+	if (wooting_rgb_kbd_connected)
+	{
+		std::cout << "Keyboard connected; beginning." << std::endl;
+	}
+	else
+	{
+		std::cout << "Keyboard not connected; terminating." << std::endl;
+	}
+
+	system("pause");
 	return 0;
 }
