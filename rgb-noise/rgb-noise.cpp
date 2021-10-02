124fb51dcb7e59e7750399e162a7f79111048713
John Connor <jckusho@gmail.com>
2021-10-02 14:00:41 -0400

    Works with Perlin noise now

--- rgb-noise/rgb-noise.cpp
+++ rgb-noise/rgb-noise.cpp
@@ -1,8 +1,9 @@
-#include <Windows.h>
 #include "rgb-noise.h"
 
-#define RAND_MAX 255
-#define rand() std::rand()
+#define RAND_MAX 4096
+
+constexpr double NOISE_SCALE = 2.0;
+constexpr double NOISE_SPEED = 0.05;
 
 int main()
 {
@@ -19,19 +20,22 @@ int main()
 			<< ", max_columns = " << (int)meta->max_columns
 			<< std::endl;
 
+		PerlinNoise noise;
+		double time = 0.0;
+
 		while (true)
 		{
 			for (uint8_t row = 0; row < meta->max_rows; row++)
 			{
 				for (uint8_t column = 0; column < meta->max_columns; column++)
 				{
-					wooting_rgb_array_set_single(row, column, rand(), rand(), rand());
+					double val = int(noise.noise(column / NOISE_SCALE, row / NOISE_SCALE, time) * 255.0);
+					wooting_rgb_array_set_single(row, column, val, val, val);
 				}
 			}
 
 			wooting_rgb_array_update_keyboard();
-			
-			Sleep(50uL);
+			time += NOISE_SPEED;
 		}
 	}
 	else
@@ -39,12 +43,18 @@ int main()
 		std::cout << "Keyboard not connected; terminating." << std::endl;
 	}
 
+	wooting_rgb_close();
+
 	system("pause");
 	return 0;
 }
 
 void exiting()
 {
-	if (wooting_rgb_kbd_connected)
-		wooting_rgb_close();
+	wooting_rgb_close();
+}
+
+int rand(int max)
+{
+	return std::rand() % max;
 }
--- rgb-noise/rgb-noise.h
+++ rgb-noise/rgb-noise.h
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <cstdlib>
 #include "wooting-rgb-sdk.h"
+#include "perlin.h"
 
 void exiting();
+
+int rand(int);
